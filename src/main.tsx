15e66ffde87974db3ea1fc872cf115ddce044a4a
Atobon28 <anita.tobon28@gmail.com>
2026-06-17 11:42:42 -0600

    Importar ajuste de cuatro enlaces

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles/index.css';
 import './styles/pages/welcomeOverrides.css';
 import './styles/pages/introTypographyMatch.css';
 import './styles/pages/navLineFix.css';
+import './styles/pages/linksFourFix.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
