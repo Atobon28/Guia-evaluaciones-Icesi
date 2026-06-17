dcdab8462c281fe158493af127eea3e1c60f0573
Atobon28 <anita.tobon28@gmail.com>
2026-06-17 11:40:08 -0600

    Crear boton de guia completa

---
+++ src/components/DownloadGuideButton.tsx
@@ -0,0 +1,8 @@
+export default function DownloadGuideButton() {
+  return (
+    <a className="btn btn-ghost download-guide-btn" href="/guia-evaluacion-ia-v2.pdf" target="_blank" rel="noreferrer">
+      <span>↓</span>
+      Descarga Guía Completa
+    </a>
+  );
+}
