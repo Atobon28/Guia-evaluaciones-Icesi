c0f2a898dd475ce862548b2589ba5a48993db565
Atobon28 <anita.tobon28@gmail.com>
2026-06-17 12:02:09 -0600

    Activar menu hamburguesa mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import MobileMenuInjector from './components/MobileMenuInjector';
 import WelcomeDownloadInjector from './components/WelcomeDownloadInjector';
 import GenericIntro from './pages/GenericIntro';
 import Principles from './pages/Principles';
@@ -23,34 +24,37 @@ function WelcomePage() {
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/por-que-redisenar" element={<GenericIntro />} />
-        <Route path="/organizacion" element={<GenericIntro />} />
-        <Route path="/principios" element={<Principles />} />
-        <Route path="/principios/conocer" element={<Principles />} />
-        <Route path="/claves-pedagogicas" element={<PedagogicalKeys />} />
-        <Route path="/ruta-redisenio" element={<RedesignRoute />} />
-        <Route path="/ruta-redisenio/paso-1" element={<StepPage />} />
-        <Route path="/ruta-redisenio/paso-2" element={<StepPage />} />
-        <Route path="/ruta-redisenio/paso-3" element={<StepPage />} />
-        <Route path="/ruta-redisenio/paso-4" element={<StepPage />} />
-        <Route path="/ruta-redisenio/paso-5" element={<StepPage />} />
-        <Route path="/ruta-redisenio/paso-6" element={<StepPage />} />
-        <Route path="/herramientas" element={<Toolbox />} />
-        <Route path="/herramientas/semaforo-ia" element={<TrafficLightAI />} />
-        <Route path="/herramientas/selector-aias" element={<AIASSelector />} />
-        <Route path="/herramientas/plantillas" element={<Templates />} />
-        <Route path="/herramientas/diagnostico" element={<Templates />} />
-        <Route path="/herramientas/ruta-cognitiva" element={<Templates />} />
-        <Route path="/herramientas/rediseno-tarea" element={<Templates />} />
-        <Route path="/herramientas/rubrica" element={<Templates />} />
-        <Route path="/herramientas/retroalimentacion" element={<Templates />} />
-        <Route path="/herramientas/checklist-final" element={<Templates />} />
-        <Route path="/cierre" element={<Closing />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <>
+      <MobileMenuInjector />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/por-que-redisenar" element={<GenericIntro />} />
+          <Route path="/organizacion" element={<GenericIntro />} />
+          <Route path="/principios" element={<Principles />} />
+          <Route path="/principios/conocer" element={<Principles />} />
+          <Route path="/claves-pedagogicas" element={<PedagogicalKeys />} />
+          <Route path="/ruta-redisenio" element={<RedesignRoute />} />
+          <Route path="/ruta-redisenio/paso-1" element={<StepPage />} />
+          <Route path="/ruta-redisenio/paso-2" element={<StepPage />} />
+          <Route path="/ruta-redisenio/paso-3" element={<StepPage />} />
+          <Route path="/ruta-redisenio/paso-4" element={<StepPage />} />
+          <Route path="/ruta-redisenio/paso-5" element={<StepPage />} />
+          <Route path="/ruta-redisenio/paso-6" element={<StepPage />} />
+          <Route path="/herramientas" element={<Toolbox />} />
+          <Route path="/herramientas/semaforo-ia" element={<TrafficLightAI />} />
+          <Route path="/herramientas/selector-aias" element={<AIASSelector />} />
+          <Route path="/herramientas/plantillas" element={<Templates />} />
+          <Route path="/herramientas/diagnostico" element={<Templates />} />
+          <Route path="/herramientas/ruta-cognitiva" element={<Templates />} />
+          <Route path="/herramientas/rediseno-tarea" element={<Templates />} />
+          <Route path="/herramientas/rubrica" element={<Templates />} />
+          <Route path="/herramientas/retroalimentacion" element={<Templates />} />
+          <Route path="/herramientas/checklist-final" element={<Templates />} />
+          <Route path="/cierre" element={<Closing />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
